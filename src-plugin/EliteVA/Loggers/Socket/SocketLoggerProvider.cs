1c9ab1f0f4d78ea4fc27912a353791a16cda19a1
moorsalin <moorsalin.munshi@gmail.com>
2024-11-27 21:55:56 -0500

    Update SocketLoggerProvider.cs

--- src-plugin/EliteVA/Loggers/Socket/SocketLoggerProvider.cs
+++ src-plugin/EliteVA/Loggers/Socket/SocketLoggerProvider.cs
@@ -1,4 +1,4 @@
-﻿using System.Net;
+using System.Net;
 using Microsoft.Extensions.Logging;
 using WatsonWebsocket;
 
@@ -7,28 +7,12 @@ namespace EliteVA.Loggers.Socket;
 public class SocketLoggerProvider : ILoggerProvider
 {
     private readonly WatsonWsServer _server;
-    private int _basePort = 58888;
-    private int _maxRetries = 10;
 
     public SocketLoggerProvider()
     {
-        for (int attempt = 0; attempt < _maxRetries; attempt++)
-            {
-                try
-                {
-                    int currentPort = _basePort + attempt;
-                    _server = new WatsonWsServer(IPAddress.Loopback.ToString(), currentPort);
-                    _server.Start();
-                    Console.WriteLine($"Server started successfully on port {currentPort}");
-                    return;
-                }
-                catch (Exception ex) when (ex is System.Net.Sockets.SocketException || ex is System.Net.HttpListenerException)
-                {
-                    Console.WriteLine($"Port {_basePort + attempt} is in use. Trying next port...");
-                }
-            }
+        _server = new WatsonWsServer(IPAddress.Loopback.ToString(), 0);
+        _server.StartAsync().GetAwaiter().GetResult();
     }
-
     
     public void Dispose()
     {
